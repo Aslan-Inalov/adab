4413bcc86b92dea1d4dec4af2759f2ad9165dc82
TamerlanCH <timur-ch@yandex.ru>
2024-02-23 22:40:19 +0300

    add hood-with fix6

--- catalog-uniform.js
+++ catalog-uniform.js
@@ -1,21 +1,12 @@
 const products = [
     {
-        url: './card-pages/tactical-uniform-black.html',
-        category: 'формы',
-        image: './images/tactical-uniform-black.jpg',
-        title: 'Мужская тактическая форма ADAB, чёрная',
-        price: '7 990₽',
-        currency: '(2 300₺ / 75$)',
-        size: 'S, M, L, XL, XXL'
-    },
-    {
         url: './card-pages/uniform-white-summer.html',
         sale: '-20%',
         category: 'формы',
         image: './images/uniform-white-summer.jpg',
         title: 'Мужская форма ADAB, лето белая',
         price: '6 392₽',
-        currency: '(1 400₺ / 60$)',
+        currency: '(1 800₺ / 60$)',
         size: 'S, M, L, XL, XXL'
     },
     {
@@ -24,7 +15,7 @@ const products = [
         image: './images/uniform-dark-blue.jpg',
         title: 'Мужская форма ADAB, темно-синяя',
         price: '7 990₽',
-        currency: '(1 750₺ / 75$)',
+        currency: '(2 300₺ / 75$)',
         size: 'S, M, L, XL, XXL'
     },
     {
@@ -33,7 +24,7 @@ const products = [
         image: './images/uniform-black.jpg',
         title: 'Мужская форма ADAB, чёрная',
         price: '7 990₽',
-        currency: '(1 750₺ / 75$)',
+        currency: '(2 300₺ / 75$)',
         size: 'M, L, XL'
     },
     {
@@ -43,7 +34,7 @@ const products = [
         image: './images/uniform-white.jpg',
         title: 'Мужская форма ADAB, осень белая',
         price: '6 392₽',
-        currency: '(1 400₺ / 60$)',
+        currency: '(1 800₺ / 60$)',
         size: 'S, M, L, XL, XXL'
     },
 ];
